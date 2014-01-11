9f4e487417dc0bba5fbc3c9c9ccdea9a2fa643ae
Eithan Shavit <eithanshavit@gmail.com>
2014-01-10 16:58:08 -0800

    Add woodwork 3lights post

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -52,9 +52,9 @@ esApp.controller('MainCtrl', ['$rootScope', '$location', '$anchorScroll', '$rout
          'style': 'nav-photography'
       },
       { 
-         'name': 'woodcraft',
-         'url': 'woodcraft',
-         'style': 'nav-woodcraft'
+         'name': 'woodwork',
+         'url': 'woodwork',
+         'style': 'nav-woodwork'
       },
       { 
          'name': 'github',
@@ -77,6 +77,10 @@ esApp.controller('MainCtrl', ['$rootScope', '$location', '$anchorScroll', '$rout
 
    $http.get('models/gallery_verbalClock.json').success(function(data) {
        $scope.verbalClockImages = data;
+   });
+
+   $http.get('models/gallery_woodwork.json').success(function(data) {
+       $scope.woodworkImages = data;
    });
    $scope.isButtonActive = function(buttonName){
       return buttonName == $routeParams['categoryId'];
